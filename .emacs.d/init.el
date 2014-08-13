d5df53812b79d26fcaf86fce040b0a3857fe9e22
Shigeki Karita <shigekikarita@gmail.com>
2014-08-13 17:18:03 +0900

    add .emacs.d

---
+++ .emacs.d/init.el
@@ -0,0 +1,12 @@
+
+;; ~/.emacs.d/site-lisp 以下全部読み込み
+(let ((default-directory (expand-file-name "~/.emacs.d/elisp")))
+  (add-to-list 'load-path default-directory)
+  (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+      (normal-top-level-add-subdirs-to-load-path)))
+
+(require 'init-loader)
+(setq init-loader-show-log-after-init nil)
+(init-loader-load "~/.emacs.d/inits")
+
+(setq shell-file-name "/usr/local/bin/zsh")
